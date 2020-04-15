ae953e760d3b00d70aee1dee3eabb468cfcdda4b
陈宏葆 <chenhongbao@outlook.com>
2020-04-15 19:26:04 +0800

    Add getters to input order,

--- data-api/src/main/java/com/nabiki/hope/common/data/InputOrder.java
+++ data-api/src/main/java/com/nabiki/hope/common/data/InputOrder.java
@@ -4,7 +4,7 @@
 package com.nabiki.hope.common.data;
 
 /**
- * Order input to market. The interface has only setters.
+ * Order input to market.
  * 
  * @author Hongbao Chen
  *
@@ -12,29 +12,55 @@ package com.nabiki.hope.common.data;
 public interface InputOrder {
 	void symbol(String s);
 	
+	String symbol();
+	
 	void accountId(String s);
+	
+	String accountId();
 	
 	void price(Double d);
 	
+	Double price();
+	
 	void stopPrice(Double d);
+	
+	Double stopPrice();
 	
 	void volume(Integer i);
 	
+	Integer volume();
+	
 	void minVolume(Integer i);
+	
+	Integer minVolume();
 	
 	void direction(TokenByte t);
 	
+	TokenByte direction();
+	
 	void priceType(TokenByte t);
+	
+	TokenByte priceType();
 	
 	void offsetFlag(TokenByte t);
 	
+	TokenByte offsetFlag();
+	
 	void hedgeFlag(TokenByte t);
+	
+	TokenByte hedgeFlag();
 	
 	void timeCondition(TokenByte t);
 	
+	TokenByte timeCondition();
+	
 	void volumeCondition(TokenByte t);
 	
+	TokenByte volumeCondition();
+	
 	void contigentConditon(TokenByte t);
+	
+	TokenByte contigentConditon();
 	
 	/**
 	 * Note on the input order such as reason for the trade.
@@ -42,4 +68,6 @@ public interface InputOrder {
 	 * @param n
 	 */
 	void note(String n);
+	
+	String note();
 }
