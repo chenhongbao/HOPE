6968980b2d33ad94458d10b4268ea5369a848960
陈宏葆 <chenhongbao@outlook.com>
2020-04-19 18:46:18 +0800

    Change NewOrder from interface to class.

--- data-api/src/main/java/com/nabiki/hope/common/data/NewOrder.java
+++ data-api/src/main/java/com/nabiki/hope/common/data/NewOrder.java
@@ -1,73 +1,134 @@
-/**
- * 
- */
 package com.nabiki.hope.common.data;
 
-/**
- * Order input to market.
- * 
- * @author Hongbao Chen
- *
- */
-public interface NewOrder {
-	void symbol(String s);
-	
-	String symbol();
-	
-	void accountId(String s);
-	
-	String accountId();
-	
-	void price(Double d);
-	
-	Double price();
-	
-	void stopPrice(Double d);
-	
-	Double stopPrice();
-	
-	void volume(Integer i);
-	
-	Integer volume();
-	
-	void minVolume(Integer i);
-	
-	Integer minVolume();
-	
-	void direction(TokenByte t);
-	
-	TokenByte direction();
-	
-	void priceType(TokenByte t);
-	
-	TokenByte priceType();
-	
-	void offsetFlag(TokenByte t);
-	
-	TokenByte offsetFlag();
-	
-	void hedgeFlag(TokenByte t);
-	
-	TokenByte hedgeFlag();
-	
-	void timeCondition(TokenByte t);
-	
-	TokenByte timeCondition();
-	
-	void volumeCondition(TokenByte t);
-	
-	TokenByte volumeCondition();
-	
-	void contigentConditon(TokenByte t);
-	
-	TokenByte contigentConditon();
-	
-	/**
-	 * Note on the input order such as reason for the trade.
-	 * 
-	 * @param n
-	 */
-	void note(String n);
-	
-	String note();
+public class NewOrder {
+	String symbol;
+	String accountId;
+	Double price;
+	Double stopPrice;
+	Integer volume;
+	Integer minVolume;
+	TokenByte direction;
+	TokenByte priceType;
+	TokenByte offsetFlag;
+	TokenByte hedgeFlag;
+	TokenByte timeCondition;
+	TokenByte volumeCondition;
+	TokenByte contigentCondition;
+	String note;
+
+	public NewOrder() {
+	}
+
+	public void symbol(String s) {
+		this.symbol = s;
+	}
+
+	public String symbol() {
+		return this.symbol;
+	}
+
+	public void accountId(String s) {
+		this.accountId = s;
+	}
+
+	public String accountId() {
+		return this.accountId;
+	}
+
+	public void price(Double d) {
+		this.price = d;
+	}
+
+	public Double price() {
+		return this.price;
+	}
+
+	public void stopPrice(Double d) {
+		this.stopPrice = d;
+	}
+
+	public Double stopPrice() {
+		return this.stopPrice;
+	}
+
+	public void volume(Integer i) {
+		this.volume = i;
+	}
+
+	public Integer volume() {
+		return this.volume;
+	}
+
+	public void minVolume(Integer i) {
+		this.minVolume = i;
+	}
+
+	public Integer minVolume() {
+		return this.minVolume;
+	}
+
+	public void direction(TokenByte t) {
+		this.direction = t;
+	}
+
+	public TokenByte direction() {
+		return this.direction;
+	}
+
+	public void priceType(TokenByte t) {
+		this.priceType = t;
+	}
+
+	public TokenByte priceType() {
+		return this.priceType;
+	}
+
+	public void offsetFlag(TokenByte t) {
+		this.offsetFlag = t;
+	}
+
+	public TokenByte offsetFlag() {
+		return this.offsetFlag;
+	}
+
+	public void hedgeFlag(TokenByte t) {
+		this.hedgeFlag = t;
+	}
+
+	public TokenByte hedgeFlag() {
+		return this.hedgeFlag;
+	}
+
+	public void timeCondition(TokenByte t) {
+		this.timeCondition = t;
+	}
+
+	public TokenByte timeCondition() {
+		return this.timeCondition;
+	}
+
+	public void volumeCondition(TokenByte t) {
+		this.volumeCondition = t;
+	}
+
+	public TokenByte volumeCondition() {
+		return this.volumeCondition;
+	}
+
+	public void contigentConditon(TokenByte t) {
+		this.contigentCondition = t;
+	}
+
+	public TokenByte contigentConditon() {
+		return this.contigentCondition;
+	}
+
+	public void note(String n) {
+		this.note = n;
+	}
+
+	public String note() {
+		return this.note;
+	}
+
 }
